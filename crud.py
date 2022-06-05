31ce10b9261a1be57915213a4afcea0a7cce0912
Muhammad <muhammadyakub181@gmail.com>
2022-06-05 23:43:04 +0100

    deployed_codes

--- crud.py
+++ crud.py
@@ -12,29 +12,38 @@ from extract import extract_coindata
 from models import Crypto
 from config import cloud_engine
 from datetime import datetime
+import logging
+logger = logging.Logger('catch_all')
 
 coindata = extract_coindata()
 Session = sessionmaker(bind=cloud_engine)
 
-def load_data(data = coindata):
+def load_data(data = coindata, s = Session()):
     start = datetime.now()
-    s = Session()
+    #s = Session()
     cols = list(data[0].keys())
-    for coin in tqdm(data, desc="Inserting data into {} table".format(Crypto.__tablename__)):
-        vals = list(coin.values())
-        s.rollback()
-        query = '''INSERT INTO public."CryptoPrice" ("{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}") 
-        VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}','{}', '{}')'''\
-        .format(cols[0], cols[1], cols[2], cols[3], cols[4], cols[5], cols[6], cols[7], cols[8], cols[9], 
-        vals[0], vals[1], vals[2], vals[3], vals[4], vals[5], vals[6], vals[7], vals[8], vals[9])
-            
-        #query = query_first + query_last
-        #row = Crypto(**coin)
-        s.execute(query)
-        s.commit()
-    stop = datetime.now()  
-    s.close()
+    
+    try:
+        for coin in tqdm(data, desc="Inserting data into {} table".format(Crypto.__tablename__)):
+            vals = list(coin.values())
+            s.rollback()
+            query = '''INSERT INTO public."CryptoPrice" ("{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}") 
+            VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}','{}', '{}')'''\
+            .format(cols[0], cols[1], cols[2], cols[3], cols[4], cols[5], cols[6], cols[7], cols[8], cols[9], 
+            vals[0], vals[1], vals[2], vals[3], vals[4], vals[5], vals[6], vals[7], vals[8], vals[9])
 
-    print('Batch Load Executed!!!')
-    print("Total time: {} seconds".format(stop-start))
+            s.execute(query)
+            s.commit()
+        stop = datetime.now()  
+        
+        print('Batch Load Executed!!!')
+        print("Total time: {} seconds".format(stop-start))
+        
+    except Exception as e:
+        logger.error(e, exc_info=True)
+        print("Could not load data into database")
+        
+    finally:
+        s.close()
+        
         
